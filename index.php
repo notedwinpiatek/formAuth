d84868aab59e05fc2f45d4c85b3633134f81ffed
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-09 10:53:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php 
 
+$email = $_REQUEST['email'];
+$password = $_REQUEST['password'];
 
+$ email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
+$db = new mysqli("localhost", "root", "", "auth");
+//ręcznie
+//$q = "SELECT * FROM user WHERE email = '$email";
+//echo $q;
+//$db->query($q)
 
+//prepared statements
+$q = $db->prepare("SELECT * FROM user WHERE email = ? LIMIT 1");
+//podstaw wartości
+$q->bind_param("s", $email);
+//wykonaj
+$q->execute();
+$result = $q->get_result();
+
+$userRow = $result->fetch_assoc();
+if($userRow == null)
+var_dump($userRow)
 
 ?>
 <form action="index.php" method="get">
@@ -10,5 +29,5 @@
     <input type="email" name="email" id="emailInput">
     <label for="passwordInput">Hasło:</label>
     <input type="password" name="password" id="passwordInput">
-    <input type="submit" value="Zaloguj">
+    <input type="submit" value="Zaloguj"> 
 </form>
