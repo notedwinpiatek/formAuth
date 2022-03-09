6691226550dd7355bae1ddfcd7906dc859500ab1
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-09 10:31:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,4 +8,7 @@
 <form action="index.php" method="get">
     <label for="emailInput">Email:</label>
     <input type="email" name="email" id="emailInput">
+    <label for="passwordInput">Hasło:</label>
+    <input type="password" name="password" id="passwordInput">
+    <input type="submit" value="Zaloguj">
 </form>
